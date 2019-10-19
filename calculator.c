3a559f7d28b64e687cb70f4f7265506264f53996
TechnonHims <Himanshu3249@gmail.com>
2019-10-19 19:20:53 +0530

    Difference function

--- calculator.c
+++ calculator.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-int sum()
+int diff()
 {
    int a =5,b=6;
-   sum=a+b;
-   return sum;
-}
-int diff(int a,int b)
-{
-   return (a-b);
+   int diff=a-b;
+   return diff;
 }
 int main()
 {
    int a=5,b=6,sum,diff ,multi, div;
-   printf("Sum = %d\n",sum());
+   printf("Diff = %d\n",diff());
 }
