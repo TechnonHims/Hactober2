25cc012ac1763d2cf7cd5812df6464870b0d1f49
TechnonHims <Himanshu3249@gmail.com>
2019-10-19 19:02:44 +0530

    My calulator works

---
+++ calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sum()
+{
+   int a =5,b=6;
+   sum=a+b;
+   return sum;
+}
+int diff(int a,int b)
+{
+   return (a-b);
+}
+int main()
+{
+   int a=5,b=6,sum,diff ,multi, div;
+   printf("Sum = %d\n",sum());
+}
